d9c2a07665ddf9d7fbd94bc5c3c85ba5f0b961f6
Artem <anufriev.artem.mail@gmail.com>
2020-10-18 20:45:25 +0300

    Added profiles table

--- configs/init.sql
+++ configs/init.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL NOT NULL PRIMARY KEY,
     login TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
+    password TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS profiles (
     userID INTEGER NOT NULL,
-    name TEXT,
+    username TEXT,
     phone TEXT,
     addresses TEXT[],
-    points INTEGER,
+    points INTEGER
 
     FOREIGN KEY (userID) REFERENCES users (id)
 );
