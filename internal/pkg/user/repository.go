eb86696fcc1d751979945ad0f4e8664ebd0d22f3
Artem <anufriev.artem.mail@gmail.com>
2020-10-24 14:27:12 +0300

    Added test for users repo

--- internal/pkg/user/repository.go
+++ internal/pkg/user/repository.go
@@ -2,6 +2,7 @@ package user
 
 import "github.com/friends/internal/pkg/models"
 
+//go:generate mockgen -destination=./repo_mock.go -package=user github.com/friends/internal/pkg/user Repository
 type Repository interface {
 	Create(user models.User) (userID string, err error)
 	CheckIfUserExists(user models.User) error
